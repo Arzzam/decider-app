b3fc1f5505d203d57ddcd191258739085bcbe511
Arzzam <92790024+Arzzam@users.noreply.github.com>
2022-02-19 20:16:23 +0530

    Result Done

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -2,10 +2,29 @@ import React, { useContext, useEffect } from "react";
 import { MyContext } from "../Context";
 
 const Result = () => {
+  const context = useContext(MyContext);
+
+  useEffect(() => {
+    context.result();
+  }, []);
+
+  const goBackHandler = () => {
+    context.goTo(0);
+  };
+
   return (
     <div>
       <h3>Your Anwer is:</h3>
-      <div className="viewer">{context.state.question}</div>
+      <div className="viewer">{context.state.result}</div>
+      <div className="animate__animated animate__fadeInUp animate__delay-0.5s">
+        <hr />
+        <button className="btn" onClick={context.result}>
+          Decide Again
+        </button>
+        <button className="btn" onClick={goBackHandler}>
+          Start Over
+        </button>
+      </div>
     </div>
   );
 };
--- src/Context/index.js
+++ src/Context/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
+const list = [
+  "Yes",
+  "No",
+  "Maybe",
+  "Not sure..try again",
+  "Ask a friend",
+  "Ask your Family Members",
+];
+
 const MyContext = React.createContext();
 class MyProvider extends Component {
   state = {
     screen: 0,
     question: "",
+    result: "",
   };
 
   gotoHandler = (value) => {
@@ -15,6 +25,22 @@ class MyProvider extends Component {
     this.setState({ question: value });
   };
 
+  getRandomValue = () => {
+    return list[Math.floor(Math.random() * list.length)];
+  };
+
+  resultHandler = () => {
+    let rand = this.getRandomValue();
+
+    if (this.state.result !== "") {
+      while (rand === this.state.result) {
+        rand = this.getRandomValue();
+      }
+    }
+
+    this.setState({ result: rand });
+  };
+
   render() {
     return (
       <>
@@ -23,6 +49,7 @@ class MyProvider extends Component {
             state: this.state,
             goTo: this.gotoHandler,
             question: this.questionHandler,
+            result: this.resultHandler,
           }}
         >
           {this.props.children}
